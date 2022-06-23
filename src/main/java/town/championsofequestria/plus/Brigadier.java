7f83597a2d14d86effb7703a5e3988cb6840046f
Sydney <github@shonx.net>
2022-06-22 22:20:08 -0400

    Various source updates
    * Update to Gradle 7.3
    * Package move org.sweetiebelle -> net.shonx
    * Update to 1.19.

--- src/main/java/town/championsofequestria/plus/Brigadier.java
+++ src/main/java/town/championsofequestria/plus/Brigadier.java
@@ -9,10 +9,9 @@ import org.bukkit.command.Command;
 
 import com.mojang.brigadier.tree.LiteralCommandNode;
 
-import me.lucko.commodore.BrigadierUnsupportedException;
 import me.lucko.commodore.Commodore;
 import me.lucko.commodore.CommodoreProvider;
-import me.lucko.commodore.file.CommodoreFileFormat;
+import me.lucko.commodore.file.CommodoreFileReader;
 
 class Brigadier {
 
@@ -22,11 +21,11 @@ class Brigadier {
     Brigadier(PlusPlugin plugin) {
         this.plugin = Objects.requireNonNull(plugin);
         this.commodore = getCommodore();
-        if(commodore.isEmpty()) {
+        if (commodore.isEmpty()) {
             plugin.getLogger().severe("Commodore is currently not available. This sucks ass!!! :(");
         }
     }
-    
+
     public boolean isAvailable() {
         return commodore.isPresent();
     }
@@ -37,7 +36,7 @@ class Brigadier {
             InputStream is = plugin.getResource(command.getName() + ".commodore");
             if (is == null)
                 throw new IOException("Brigadier command data missing from jar.");
-            LiteralCommandNode<?> commandNode = CommodoreFileFormat.parse(is);
+            LiteralCommandNode<?> commandNode = CommodoreFileReader.INSTANCE.parse(is);
             commodore.register(command, commandNode);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
@@ -46,8 +45,10 @@ class Brigadier {
 
     private Optional<Commodore> getCommodore() {
         try {
-            return Optional.ofNullable(CommodoreProvider.getCommodore(plugin));
-        } catch (BrigadierUnsupportedException e) {
+            if (CommodoreProvider.isSupported())
+                return Optional.ofNullable(CommodoreProvider.getCommodore(plugin));
+            return Optional.empty();
+        } catch (Exception e) {
             return Optional.empty();
         }
     }
